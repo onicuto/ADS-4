4cb71233bd0a0ad73e130d59d897c6eb75b64603
onicuto <kiryauvachan@gmail.com>
2026-03-25 19:33:35 +0300

    ADS-4

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,35 +45,44 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
+
   for (int i = 0; i < len; i++) {
     int target = value - arr[i];
+
     int left = i + 1;
     int right = len - 1;
-    int pos = -1;
+    int first = -1;
+
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (arr[mid] == target) {
-        pos = mid;
-        break;
-      } else if (arr[mid] < target) {
-        left = mid + 1;
-      } else {
+      if (arr[mid] >= target) {
+        if (arr[mid] == target) {
+          first = mid;
+        }
         right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
-    if (pos != -1) {
-      int first = pos;
-      while (first > i && arr[first - 1] == target) {
-        first--;
+
+    if (first != -1) {
+      left = first;
+      right = len - 1;
+      int last = first;
+
+      while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] <= target) {
+          if (arr[mid] == target) {
+            last = mid;
+          }
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
       }
-      int last = pos;
-      while (last + 1 < len && arr[last + 1] == target) {
-        last++;
-      }
+
       count += (last - first + 1);
-      while (i + 1 < len && arr[i + 1] == arr[i]) {
-        i++;
-      }
     }
   }
 
